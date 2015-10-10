6f8d36b387a60de7cd38d9d2c81e8d7e185a2471
Rachel Marison <rachel.marison@tufts.edu>
2015-10-09 20:01:51 -0400

    Added in background image

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,16 @@
-// Replace this with your code...
+//game.js
+//by Rachel Marison
+
+function init() {
+var canvas = document.getElementById("game_canvas");
+var ctx = canvas.getContext("2d");
+
+var backgroundImage = new Image();
+backgroundImage.addEventListener("load", function() {
+	ctx.drawImage(backgroundImage, 0, 0, 800, 600);
+}, false);
+backgroundImage.src = 'duckhunt-background.gif'
+
+ctx.fillStyle = "green";
+ctx.fillRect(0, 0, 100, 100);
+}
