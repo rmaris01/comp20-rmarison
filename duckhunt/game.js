15ca2d0e751d2ed39e17fbbc31df67d65c3cd948
Rachel Marison <rachel.marison@tufts.edu>
2015-10-11 20:51:10 -0400

    Added 2 ducks to canvas

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -6,11 +6,16 @@ var canvas = document.getElementById("game_canvas");
 var ctx = canvas.getContext("2d");
 
 var backgroundImage = new Image();
-backgroundImage.addEventListener("load", function() {
-	ctx.drawImage(backgroundImage, 0, 0, 800, 600);
-}, false);
 backgroundImage.src = 'duckhunt-background.gif'
 
-ctx.fillStyle = "green";
-ctx.fillRect(0, 0, 100, 100);
+var ducks = new Image();
+ducks.src = 'duckhunt_various_sheet.png'
+
+backgroundImage.addEventListener("load", function() {
+	ctx.drawImage(backgroundImage, 0, 0, 800, 600);
+	ctx.drawImage(ducks, 39, 122, 36, 22, 225, 170, 108, 66);
+	ctx.drawImage(ducks, 129, 120, 36, 26, 425, 100, 108, 78);
+
+}, false);
+
 }
